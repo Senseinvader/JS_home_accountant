e98fe39d2ca9908e3182bd74c98e803ab7c48c92
Senseinvader <d.ivantsov1983@gmail.com>
2018-10-25 21:29:58 +0200

    Add mongoDB, mongoose and mocha, chai dependencies, rewrite db connection to mongoDB instead

--- server.js
+++ server.js
@@ -1,8 +1,11 @@
 "strict mode"
 
 const hapi = require('hapi');
-const pg = require('pg');
+// const pg = require('pg');
+const mongoose = require('mongoose');
 
+
+//Create server connection
 const server = hapi.server({
   port : 8000,
   host : 'localhost'
@@ -20,6 +23,16 @@ const init = async () => {
   await server.start();
   console.log(`server runs at: ${server.info.uri}`);
 };
+init();
+
+//Create connection to MongoDB database
+mongoose.connect('mongodb://localhost/accountant');
+
+mongoose.connection.once('open', function() {
+  console.log('DB connection has been established');
+}).on('error', function(error) {
+  console.log('Connection error: ', error);
+});
 
 // const connectionString = 'postgresql://postgres:dima1234@localhost:5432/accountantDB';
 //   const Client = pg.Client;
@@ -30,4 +43,3 @@ const init = async () => {
 //   client.end();
 // });
 
-init();
