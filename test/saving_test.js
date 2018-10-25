c8e23ab743b9b8bbc21463073b896015b94f6d60
Senseinvader <d.ivantsov1983@gmail.com>
2018-10-25 23:29:00 +0200

    Create test file to test CRUD operations on mongoDB with test saving records

---
+++ test/saving_test.js
@@ -0,0 +1,19 @@
+const mocha = require('mocha');
+const assert = require('chai').assert;
+const Expense = require('../model/Expense');
+
+describe('Saving records', function() {
+  
+  it('Saves records to the database', function(done) {
+    var newExpence = new Expense({
+      amount: 100,
+      date: Date.parse("11/30/2011"),
+      comment: 'No comment',
+      category: 'Food'
+    });
+    newExpence.save().then(function() {
+      assert(!newExpence.isNew);
+      done();
+    });
+  });
+});
