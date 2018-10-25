4421ec4718ed0ecc1763b7616ae91b494645bf06
Senseinvader <d.ivantsov1983@gmail.com>
2018-10-25 21:42:05 +0200

    Create a testing environment (works)

---
+++ test/demo_test.js
@@ -0,0 +1,9 @@
+const mocha = require('mocha');
+const assert = require('chai').assert;
+
+describe('do some tests', function() {
+  
+  it('does smth particular', function() {
+    assert.equal(1+1, 2);
+  });
+});
