4f26f998469d93fee5f2ff4b63c359e1ccb1d600
Senseinvader <d.ivantsov1983@gmail.com>
2018-10-25 21:41:11 +0200

    Add testing script to package json

--- src/code/model/Expense.js
+++ src/code/model/Expense.js
@@ -1,42 +1,56 @@
+const mongoose = require('mongoose');
 
-export default class Expense {
+const Schema = mongoose.Schema;
+
+const ExpenseSchema = new Schema({
+  amount: Number,
+  date: Date,
+  comment: String,
+  category: String
+});
+
+const Expense = mongoose.model('expenses', ExpenseSchema);
+
+module.exports = Expense;
+
+// export default class Expense {
   
-  constructor (amount, date, comment, category) {
-    this.amout = amount;
-    this.date = date;
-    this.comment = comment;
-    this.category = category;
-  }
+//   constructor (amount, date, comment, category) {
+//     this.amout = amount;
+//     this.date = date;
+//     this.comment = comment;
+//     this.category = category;
+//   }
 
-  get amount () {
-    return this.amount;
-  }
+//   get amount () {
+//     return this.amount;
+//   }
 
-  set amount(amount) {
-    this.amount = amount;
-  }
+//   set amount(amount) {
+//     this.amount = amount;
+//   }
 
-  get date () {
-    return this.date;
-  }
+//   get date () {
+//     return this.date;
+//   }
 
-  set date(date) {
-    this.date = date;
-  }
+//   set date(date) {
+//     this.date = date;
+//   }
 
-  get comment () {
-    return this.comment;
-  }
+//   get comment () {
+//     return this.comment;
+//   }
 
-  set comment(comment) {
-    this.comment = comment;
-  }
+//   set comment(comment) {
+//     this.comment = comment;
+//   }
 
-  get category () {
-    return this.category;
-  }
+//   get category () {
+//     return this.category;
+//   }
 
-  set category(category) {
-    this.category = category;
-  }  
-}
+//   set category(category) {
+//     this.category = category;
+//   }  
+// }
